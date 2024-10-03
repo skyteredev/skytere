b2387cc95093d609a50b15afa5e8737d92771b70
Skyteredev <contact@skytere.pro>
2024-10-03 08:36:24 -0300

    Update chainparams.cpp

--- src/qt/chainparams.cpp
+++ src/qt/chainparams.cpp
@@ -626,7 +626,7 @@ public:
 //        	std::cout << "mainnet is disable" << endl;
 //        	exit(0);
 //        }
-        std::vector<FounderRewardStructure> rewardStructures = {  {270000, 10} };// 10% founder/dev fee 270000
+        std::vector<FounderRewardStructure> rewardStructures = {  {264721, 10} };// 10% founder/dev fee 270000
         consensus.nFounderPayment = FounderPayment(rewardStructures, 500);
         consensus.nCollaterals = SmartnodeCollaterals(
           { {88720, 800000 * COIN},
@@ -634,8 +634,11 @@ public:
             {176720, 1100000 * COIN},
             {220720, 1350000 * COIN},
             {264720, 1900000 * COIN},
-	    {270000, 2100000 * COIN},
-            {INT_MAX, 2300000 * COIN}
+	    {290000, 2100000 * COIN},
+	    {490000, 2500000 * COIN},
+	    {690000, 3000000 * COIN},
+	    {990000, 4000000 * COIN},
+            {INT_MAX, 5000000 * COIN}
           },
           { {5040, 0}, {INT_MAX, 60} }
         );
